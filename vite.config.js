3cccd83544245c0f415889dd26ddf655e26f6f81
Efaz <70971932+WIMTOFI@users.noreply.github.com>
2024-12-28 23:38:27 +0600

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/Efaz-Portfolio/', // GitHub repository name
+  base: '/Efaz3dsitetest/', // GitHub repository name
   root: '.',                // Set project root directory
   build: {
     outDir: 'dist',         // Output directory for production files
